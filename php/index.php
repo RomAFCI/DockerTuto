631d5c63d9fa88999340b0ce685aa5a38d32620d
RomAFCI <adnot.rom@gmail.com>
2025-07-04 14:39:36 +0200

    save

--- php/index.php
+++ php/index.php
@@ -1,3 +1,18 @@
 <?php
+$host = 'db';
+$dbname = 'bddCreateOnPhpMyAdmin';
+$user = 'root';
+$password = 'root';
 
-phpinfo();
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+    // Active les erreurs PDO en exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connexion réussie !";
+} catch (PDOException $e) {
+    echo "Erreur de connexion : " . $e->getMessage();
+}
+
+
+
+?>
